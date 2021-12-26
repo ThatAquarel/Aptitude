f391494bff5ade9ec30031d023be64a5d951054e
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-12-26 16:09:34 -0500

    4 characters

--- drafts/stat_intersect.py
+++ drafts/stat_intersect.py
@@ -16,44 +16,70 @@ def get_probabilistic_intersection(sets) -> dict[str, float]:
         ])
         return np.average(combination_count, weights=global_counts, axis=-1)
 
-    x, y = np.meshgrid(unique_chars, unique_chars)
-    char_combinations = np.char.add(x, y).reshape(-1)
+    permute_x, permute_y = np.meshgrid(unique_chars, unique_chars)
+    char_combinations = np.char.add(permute_x, permute_y).reshape(-1)
 
     char_combinations_probabilities = get_combination_probabilities(char_combinations)
     variation = round(weighted_average_length)
     probable_indices = np.argpartition(char_combinations_probabilities, -variation)[-variation:]
 
-    probable_char_combinations = char_combinations[probable_indices]
-    probable_char_probabilities = char_combinations_probabilities[probable_indices]
-    probable_separated_chars = [[char for char in string] for string in probable_char_combinations]
+    intersect_combinations = char_combinations[probable_indices]
+    intersect_combinations_probabilities = char_combinations_probabilities[probable_indices]
 
-    if np.all(probable_char_probabilities == probable_char_probabilities[0]):
+    if np.all(intersect_combinations_probabilities == intersect_combinations_probabilities[0]):
         raise Exception("No two character intersection")
 
     if weighted_average_length < 3:
         return {
             combination: probability
             for combination, probability in
-            zip(probable_char_combinations, probable_char_probabilities)
+            zip(intersect_combinations, intersect_combinations_probabilities)
         }
 
-    intersect_combinations = []
-    for i, x in enumerate(probable_char_combinations):
-        for j, y in enumerate(probable_separated_chars):
-            if i == j:
-                continue
-            if y[0] in x:
-                intersect_combinations.append(f"{x}{y[1]}")
-            elif y[1] in x:
-                intersect_combinations.append(f"{y[0]}{x}")
-    intersect_combinations = np.unique(intersect_combinations)
+    def get_intersections(combinations):
+        intersections = []
+        for i, x in enumerate(combinations):
+            for j, y in enumerate(combinations):
+                if i == j:
+                    continue
+                y = [char for char in y]
+
+                if y[0] == x[-1]:
+                    intersections.append(f"{x}{''.join(y[1:])}")
+                elif y[-1] == x[0]:
+                    intersections.append(f"{''.join(y[:-1])}{x}")
+
+                if ''.join(y[:-1]) == x[1:]:
+                    intersections.append(f"{x}{y[-1]}")
+                elif ''.join(y[1:]) == x[:-1]:
+                    intersections.append(f"{y[0]}{x}")
+
+        return np.unique(intersections)
+
+    intersect_combinations = get_intersections(intersect_combinations)
     intersect_combinations_probabilities = get_combination_probabilities(intersect_combinations)
 
-    return {
-        combination: probability
-        for combination, probability in
-        zip(intersect_combinations, intersect_combinations_probabilities)
-    }
+    if np.unique(intersect_combinations_probabilities).shape[0] == intersect_combinations_probabilities.shape[0]:
+        return {
+            combination: probability
+            for combination, probability in
+            zip(intersect_combinations, intersect_combinations_probabilities)
+        }
+
+    # get indices of two duplicated probabilities
+    idx_sort = np.argsort(intersect_combinations_probabilities)
+    sorted_records_array = intersect_combinations_probabilities[idx_sort]
+    values, idx_start, count = np.unique(sorted_records_array, return_counts=True, return_index=True)
+    values = values[count > 1]
+    res = np.split(idx_sort, idx_start[1:])
+    res = list(filter(lambda x: x.size > 1, res))[0]
+
+    intersect_combinations = intersect_combinations[res]
+    intersect_combinations = get_intersections(intersect_combinations)
+
+    if len(intersect_combinations) != 1:
+        raise Exception("Illegal state")
+    return {intersect_combinations[0]: values[0]}
 
 
 def get_probable_string(probabilistic_intersection: dict[str, float]) -> str:
@@ -66,9 +92,23 @@ def get_probable_string(probabilistic_intersection: dict[str, float]) -> str:
 
 def main():
     print(get_probable_string(get_probabilistic_intersection({
+        "abcde": 30,
+        "febcde": 15,
+        "bcdefgh": 2,
+        "cd": 5
+    })))  # bcde
+
+    print(get_probable_string(get_probabilistic_intersection({
         "abcd": 30,
         "bcde": 15,
         "bcd": 2,
+        "cd": 5
+    })))  # bcd
+
+    print(get_probable_string(get_probabilistic_intersection({
+        "abcde": 30,
+        "febcde": 15,
+        "bcdefgh": 2,
         "cd": 5
     })))  # bcd
 

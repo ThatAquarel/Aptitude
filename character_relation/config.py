5d9125f98922d196f9c4a1752b2eb19f8731369c
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2022-01-02 18:15:43 -0500

    Update for zh

--- character_relation/config.py
+++ character_relation/config.py
@@ -1,4 +1,5 @@
 MAX_TOKEN_LENGTH = 32
 
-SPACY_NLP_MODEL = "en_core_web_md"
+# SPACY_NLP_MODEL = "en_core_web_md"
+SPACY_NLP_MODEL = "zh_core_web_lg"
 SPACY_WORD_VEC_SIZE = 300
--- character_relation/infer.py
+++ character_relation/infer.py
@@ -6,16 +6,19 @@ model = CharacterRelationModel()
 
 keras_model = keras.models.load_model("character_relation_model")
 
+# test_sentences = [
+#     "My boss at work is extremely toxic, he insults everyone.",
+#     "He's my accomplice.",
+#     "He's my rival in a swimming competition",
+#     "Romeo and Juliet, the two secret lovers of the Shakespeare era.",
+#     "The love and hate relationship that I have with her.",
+#     "My coworker and I work very efficiently together.",
+#     "He's so boring and always has a serious tone.",
+#     "A social media influencer has many attracted and dedicated fans.",
+#     "They kissed in front of everyone at a party."
+# ]
 test_sentences = [
-    "My boss at work is extremely toxic, he insults everyone.",
-    "He's my accomplice.",
-    "He's my rival in a swimming competition",
-    "Romeo and Juliet, the two secret lovers of the Shakespeare era.",
-    "The love and hate relationship that I have with her.",
-    "My coworker and I work very efficiently together.",
-    "He's so boring and always has a serious tone.",
-    "A social media influencer has many attracted and dedicated fans.",
-    "They kissed in front of everyone at a party."
+    "我的老板在工作中非常不愉快，他侮辱每个人。"
 ]
 y = keras_model.predict(model.preprocess(test_sentences))
 print(y)

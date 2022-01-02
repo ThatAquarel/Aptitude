6dae6a5448fafca78031e2f2e783b2e8cd47dad2
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2022-01-02 15:20:03 -0500

    New model

--- character_relation/infer.py
+++ character_relation/infer.py
@@ -5,10 +5,17 @@ from character_relation.model import CharacterRelationModel
 model = CharacterRelationModel()
 
 keras_model = keras.models.load_model("character_relation_model")
-y = keras_model.predict(model.preprocess([
-    "My boss at work is extremely toxic, he insults everyone.",
-    "He's my accomplice",
-    "He's my rival in a swimming competition"
-]))
 
+test_sentences = [
+    "My boss at work is extremely toxic, he insults everyone.",
+    "He's my accomplice.",
+    "He's my rival in a swimming competition",
+    "Romeo and Juliet, the two secret lovers of the Shakespeare era.",
+    "The love and hate relationship that I have with her.",
+    "My coworker and I work very efficiently together.",
+    "He's so boring and always has a serious tone.",
+    "A social media influencer has many attracted and dedicated fans.",
+    "They kissed in front of everyone at a party."
+]
+y = keras_model.predict(model.preprocess(test_sentences))
 print(y)
